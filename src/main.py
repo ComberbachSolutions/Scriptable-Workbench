67daf894d9e0946fb5403572002ab5356664f9a8
Craig Comberbach <craig.comberbach@gmail.com>
2024-05-09 12:48:14 -0500

    Setup for Gradient H2 Control

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ def Display_Live_Values():
         print(f"Set to\t{dev.CH2.get_voltage()}V\t{dev.CH2.get_current()}A")
         print(f"Actual\t{dev.CH2.measure_voltage()}V\t{dev.CH2.measure_current()}A\t{dev.CH2.measure_power()}W")
 
-def Setup_For_MUA():
+def Setup_For_Gradient_MUA():
     with spd3303x.EthernetDevice("169.254.140.1") as dev:
         dev.CH1.set_output(False)
         # dev.CH1.set_voltage(3.3)
@@ -37,3 +37,22 @@ def Setup_For_MUA():
     Delay_In_Seconds(1.0)
     Display_Live_Values()
 
+def Setup_For_Gradient_H2_Control():
+    with spd3303x.EthernetDevice("169.254.140.1") as dev:
+        dev.CH1.set_output(False)
+        dev.CH1.set_voltage(12.0)
+        dev.CH1.set_current(0.05)
+        dev.CH1.set_output(True)
+
+        dev.CH2.set_output(False)
+        dev.CH2.set_voltage(24.0)
+        dev.CH2.set_current(0.05)
+        dev.CH2.set_output(True)
+
+        # It is not used, but turning it allows me to manually toggle all supplies on/off
+        dev.CH3.set_output(True)
+    Delay_In_Seconds(1.0)
+    Display_Live_Values()
+
+# Setup_For_Gradient_MUA()
+Setup_For_Gradient_H2_Control()
